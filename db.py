04c1427de1324b1d21c6edfc36629a6ec69de862
Cheong Khai Yun <I22023337@student.newinti.edu.my>
2026-03-03 16:53:39 +0800

    Hide FIle

--- db.py
+++ db.py
@@ -1,12 +1,14 @@
-
+import os
 import psycopg2
-from psycopg2.extras import RealDictCursor
+from dotenv import load_dotenv
+
+load_dotenv()  # loads .env into environment variables
 
 def get_db_connection():
-    conn = psycopg2.connect(
-        host="localhost",
-        database="InsightHubDB",
-        user="postgres",
-        password="Cky20040505"
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST", "localhost"),
+        database=os.getenv("DB_NAME", "InsightHubDB"),
+        user=os.getenv("DB_USER", "postgres"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT", "5432"),
     )
-    return conn
